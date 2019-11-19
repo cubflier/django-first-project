b7c74b198f79dba42082c3812531693539e133d2
Jerry Kallam <jerrykallam@Livingroom-iMac.local>
2019-11-19 14:02:22 -0900

    second

--- second_project/second_app/views.py
+++ second_project/second_app/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<em>My Second Resonse</em>')
+    return HttpResponse('<em>My Second Resonse as it was changed</em>')
 
 # Create your views here.
